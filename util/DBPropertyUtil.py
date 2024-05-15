6617f643611e91eb408f3cbc21e9c3e797726710
manju1910 <manjusubramaniam02@gmail.com>
2024-05-16 05:09:46 +0530

    solved error in util package

--- util/DBPropertyUtil.py
+++ util/DBPropertyUtil.py
@@ -1,16 +1,16 @@
 class PropertyUtil:
 
-@staticmethod
-def   get_property_string():
-        server_name= "DESKTOP-0EUUQEO\\SQLEXPRESS"
-        database_name = "SISDB"
+    @staticmethod
+    def   get_property_string():
+            server_name= "DESKTOP-0EUUQEO\\SQLEXPRESS"
+            database_name = "SISDB"
+        
+        
+            conn_str = (
+                f"Driver={{SQL Server}};"
+                f"Server={server_name};"
+                f"Database={database_name};"
+                f"Trusted_Connection=yes;"
+            )
     
-    
-        conn_str = (
-            f"Driver={{SQL Server}};"
-            f"Server={server_name};"
-            f"Database={database_name};"
-            f"Trusted_Connection=yes;"
-        )
- 
-        return conn_str
+            return conn_str
--- util/DBconn.py
+++ util/DBconn.py
@@ -1,10 +1,14 @@
-
 import pyodbc
+from util.DBPropertyUtil import PropertyUtil 
 
-class DBConnection:
+
+class DBconnection:
     def __init__(self):
+        conn_str=PropertyUtil.get_property_string()
         self.conn=pyodbc.connect(conn_str)
         self.cursor=self.conn.cursor()
+    
     def close(self):
         self.cursor.close()
         self.conn.close()
+           
